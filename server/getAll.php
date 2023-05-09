df89b3eaee6c5b26952ddcf9da94e0d20445026d
jenniferaa <128671261+jenniferaa@users.noreply.github.com>
2023-05-09 13:32:17 -0600

    Initial commit

---
+++ server/getAll.php
@@ -0,0 +1,8 @@
+<?php
+ ?>
+<?php
+header("Access-Control-Allow-Origin: http://localhost:4200");
+$bd = include_once "bd.php";
+$sentencia = $bd->query("select id, nombre, raza, edad from mascotas");
+$mascotas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+echo json_encode($mascotas);
